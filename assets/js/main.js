2dca7920340a17098e0101b99737a01a74243946
IgorLuíz <igorelmerico@hotmail.com>
2016-08-20 04:48:05 -0300

    Refactoring js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3129,25 +3129,55 @@ window.$ === undefined && (window.$ = Zepto)
 ;
 
 (function ($, particlesJS, window) {
-  particlesJS.load('stars', '/assets/particles.json', function () {
-    console.log('callback - particles.js config loaded')
-  })
 
+  // Snow FX
+  // =======
+  particlesJS.load('stars', '/assets/particles.json')
+
+  // Template for project list
+  // =========================
+  // res {Object}
   var template = function (res) {
     return '<a href="' + res.html_url + '" class="project">' +
               '<span class="project--star">' + res.stargazers_count +
                 '<svg class="project--staricon"><use xlink:href="#icon-star"></use></svg>' +
               '</span>' +
-              '<h1 class="project--title">' + res.name.replace(/-/g, ' ') + '</h1>' +
-              '<h2 class="project--desc">' + res.description.replace(/:.+:/g, '') + '</h2>' +
+              '<h1 class="project--title">' + pretty(res.name, 'title') + '</h1>' +
+              '<h2 class="project--desc">' + pretty(res.description, 'desc') + '</h2>' +
             '</a>'
   }
 
+  // Pretty visualization
+  // ====================
+  // value, type {String}
+  var pretty = function (value, type) {
+    switch (type) {
+
+      // Tittle
+      // ======
+      case 'title':
+        var v = value.replace(/-/g, ' ').split(' ')
+        v[0] = v[0].substr(0, 1).toUpperCase() + v[0].substr(1)
+        return v.toString().replace(/,/g, ' ')
+
+      // Description
+      // ===========
+      case 'desc':
+        return value.replace(/:.+:/g, '')
+      default:
+       return
+    }
+  }
+
+  // Load projects
+  // =============
+  // owner {String}
+  // repos {Array}
   var app = {
-    loadProjects: function (repos) {
+    loadProjects: function (owner, repos) {
       repos.forEach(function (el) {
         $.ajax({
-          url: 'https://api.github.com/repos/Halfeld/' + el,
+          url: 'https://api.github.com/repos/' + owner + '/' + el,
           type: 'GET',
           success: function (res) {
             $('.projects').append(template(res))
@@ -3160,7 +3190,7 @@ window.$ === undefined && (window.$ = Zepto)
     }
   }
 
-  app.loadProjects([
+  app.loadProjects('Halfeld', [
     'v-notes',
     'vue-component-menu',
     'react-cards',
--- src/js/zmain.js
+++ src/js/zmain.js
@@ -1,21 +1,53 @@
 (function ($, particlesJS, window) {
+
+  // Snow FX
+  // =======
   particlesJS.load('stars', '/assets/particles.json')
 
+  // Template for project list
+  // =========================
+  // res {Object}
   var template = function (res) {
     return '<a href="' + res.html_url + '" class="project">' +
               '<span class="project--star">' + res.stargazers_count +
                 '<svg class="project--staricon"><use xlink:href="#icon-star"></use></svg>' +
               '</span>' +
-              '<h1 class="project--title">' + res.name.replace(/-/g, ' ') + '</h1>' +
-              '<h2 class="project--desc">' + res.description.replace(/:.+:/g, '') + '</h2>' +
+              '<h1 class="project--title">' + pretty(res.name, 'title') + '</h1>' +
+              '<h2 class="project--desc">' + pretty(res.description, 'desc') + '</h2>' +
             '</a>'
   }
 
+  // Pretty visualization
+  // ====================
+  // value, type {String}
+  var pretty = function (value, type) {
+    switch (type) {
+
+      // Tittle
+      // ======
+      case 'title':
+        var v = value.replace(/-/g, ' ').split(' ')
+        v[0] = v[0].substr(0, 1).toUpperCase() + v[0].substr(1)
+        return v.toString().replace(/,/g, ' ')
+
+      // Description
+      // ===========
+      case 'desc':
+        return value.replace(/:.+:/g, '')
+      default:
+       return
+    }
+  }
+
+  // Load projects
+  // =============
+  // owner {String}
+  // repos {Array}
   var app = {
-    loadProjects: function (repos) {
+    loadProjects: function (owner, repos) {
       repos.forEach(function (el) {
         $.ajax({
-          url: 'https://api.github.com/repos/Halfeld/' + el,
+          url: 'https://api.github.com/repos/' + owner + '/' + el,
           type: 'GET',
           success: function (res) {
             $('.projects').append(template(res))
@@ -28,7 +60,7 @@
     }
   }
 
-  app.loadProjects([
+  app.loadProjects('Halfeld', [
     'v-notes',
     'vue-component-menu',
     'react-cards',
