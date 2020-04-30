57dd72d1b96e59b5c85e81c14ce4ee7d50689c85
Igor Halfeld <hello@igorluiz.me>
2020-04-30 07:00:42 -0300

    start refractoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,5 @@
 (async (window) => {
   const $ = element => document.querySelector(element);
-  const loadComponent = async component => {
-    const res = await fetch(`/assets/components/${component}.html`);
-    return await res.text();
-  };
-
-  const pretty = (value, type) => {
-    const types = {
-      title: () => {
-        var v = value.replace(/-/g, ' ').split(' ');
-        v[0] = v[0].substr(0, 1).toUpperCase() + v[0].substr(1);
-        return v.toString().replace(/,/g, ' ');
-      },
-      desc: () => value.replace(/:.+:/g, ''),
-    };
-    return types[type]
-      ? types[type]()
-      : null;
-  };
-
-  const template = res => (`
-    <a href="${res.html_url}" target="_blank" class="project">
-      <span class="project--star">${res.stargazers_count}
-        <svg class="project--staricon"><use xlink:href="#icon-star"></use></svg>
-      </span>
-      <h1 class="project--title">${pretty(res.name, 'title')}</h1>
-      <h2 class="project--desc">${pretty(res.description, 'desc')}</h2>
-    </a>
-  `);
-
-  // const loadProjects = owner => repos => {
-  //   const promises = repos.map(repo =>
-  //     fetch(`https://api.github.com/repos/${owner}/${repo}`)
-  //     .then(res => res.json())
-  //   );
-
-  //   Promise.all(promises)
-  //   .then(res => {
-  //     res.forEach(item => {
-  //       $('.projects').innerHTML += template(item);
-  //     })
-  //   });
-  // };
 
   const addEventListeners = () => {
     const btnMenu = $('#btn-menu');
@@ -49,18 +7,6 @@
 
     btnMenu.addEventListener('click', () => wrapper.classList.toggle('show'));
   };
-
-  // const withIgorHalfeldProjects = loadProjects('IgorHalfeld');
-  // const withBlackCapsProjects = loadProjects('blackcapz');
-  // const withRedtubeLabsProjects = loadProjects('redtubelabs');
-  // const withNOALVOProjects = loadProjects('NOALVO');
-
-  // const runLoadProjects = () => Promise.all([
-  //   withIgorHalfeldProjects(['v-notes', 'lagoinha', 'halfeld-components', 'slidefy', 'vue-2-course', 'recordall', 'negrito']),
-  //   withBlackCapsProjects(['spammer-core']),
-  //   withRedtubeLabsProjects(['vue']),
-  //   withNOALVOProjects(['vue-architecture-boilerplate']),
-  // ]);
 
   const changeContext = ({ bg, title, desc }) => {
     $('#stars').style.backgroundImage = `url(/assets/img/${bg})`;
@@ -102,54 +48,6 @@
       });
     },
   };
-
-  Vue.component('collapse', {
-    template: await loadComponent('Collapse'),
-    props: ['title'],
-    data: () => ({
-      isOpen: false,
-    }),
-    methods: {
-      toggle() {
-        this.isOpen = !this.isOpen;
-      },
-    },
-  });
-
-  // graphql-advanced-concepts
-  new Vue({
-    el: '#allow-vue',
-    delimiters: ['${', '}'],
-    data: {
-      courses: [],
-    },
-    async mounted() {
-      const [
-        graphqlAdvancedResponse,
-        introVueResponse,
-        vueInDepthResponse,
-      ] = await Promise.all([
-        fetch('/assets/courses/graphql-advanced-concepts.json'),
-        fetch('/assets/courses/intro-to-vue.json'),
-        fetch('/assets/courses/vue-in-depth.json'),
-      ]);
-      const [
-        graphqlAdvanced,
-        introVue,
-        vueInDepth,
-      ] = await Promise.all([
-        graphqlAdvancedResponse.json(),
-        introVueResponse.json(),
-        vueInDepthResponse.json(),
-      ]);
-
-      this.courses = [
-        graphqlAdvanced,
-        introVue,
-        vueInDepth,
-      ];
-    }
-  });
 
   const pathname = window.location.pathname;
   const route = pathname.replace(/\//g, '');
